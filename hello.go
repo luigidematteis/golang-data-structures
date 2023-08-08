a670fb0c54e1a5ec185c7db0be95c33c2a48c756
luigidematteis <ldematteis123@gmail.com>
2023-08-08 11:51:16 +0300

    challenges: add and solve RotatedSortedArray
    Signed-off-by: luigidematteis <ldematteis123@gmail.com>

--- hello.go
+++ hello.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"learning/arrays"
+	"learning/challenges"
 	"learning/trees"
 )
 
 func main() {
 	arrays.CallMethodsInPackageArray(false)
+	runDemoWithChallenges()
+}
 
+func runDemoWithTree() {
 	n := &trees.Node{Value: 3, Left: nil, Right: nil}
 	n.Left = &trees.Node{Value: 10, Left: nil, Right: nil}
 	n.Right = &trees.Node{Value: 26, Left: nil, Right: nil}
@@ -20,4 +24,8 @@ func main() {
 	b.Add(56)
 	b.Add(58)
 	fmt.Println(b)
+}
+
+func runDemoWithChallenges() {
+	challenges.Search([]int{4, 5, 6, 7, 0, 1, 2}, 0)
 }
